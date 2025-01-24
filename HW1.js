2f473e2c270a66af7a05f5f44dc1a0e15794873e
dimokol <dimokol28@gmail.com>
2025-01-24 13:50:29 +0200

    1 and 2 left

--- HW1.js
+++ HW1.js
@@ -1,1 +1,15 @@
+// Check if the certification has  _id
 // *hasOwnProperty
+
+const certification = {
+    _id: '3567798099455779',
+    name: 'Marios',
+    age: 45,
+    year:2005,
+    grade: 8,
+}
+
+const result = add your code here
+
+
+console.log(result);
--- HW2.js
+++ HW2.js
@@ -1,1 +1,7 @@
+// If the given input is an array of numbers, return the sum of all the positive ones. If the array is empty or there aren't any positive numbers, return 0.
 // *reduce
+const Numbers = [ 1, -4, 12, 0, -3, 29, -150];
+
+const result = add your code here
+
+console.log(result);
